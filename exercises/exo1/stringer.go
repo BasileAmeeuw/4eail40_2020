eb771cd44bfaec4cd2d5416331b034fd3c3fd977
Basile Ameeuw <basile.ameeuw@hotmail.com>
2020-10-19 17:14:17 +0200

    exo1

--- exercises/exo1/stringer.go
+++ exercises/exo1/stringer.go
@@ -3,6 +3,29 @@ package main
 import "fmt"
 
 // Implement types Rectangle, Circle and Shape
+type Rectangle struct {
+	//two attributes
+	Width  int32
+	Length int32
+}
+
+type Circle struct {
+	//one attribute
+	Radius int32
+}
+
+//now we create a string to "see" properties of Rectangle and Circle
+func (r Rectangle) String() string {
+	return fmt.Sprint("Square of width ", r.Width, " and length ", r.Length)
+}
+
+func (c Circle) String() string {
+	return fmt.Sprint("Circle of radius ", c.Radius)
+}
+
+type Shape interface { // we create an interface that could be used in Rectangle and Circle i.e.
+	fmt.Stringer
+}
 
 func main() {
 	r := &Rectangle{2, 3}
