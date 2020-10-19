9998431c1ed179a60fc2348be36416098faa61e2
Basile Ameeuw <basile.ameeuw@hotmail.com>
2020-10-19 18:53:43 +0200

    Exo2

--- exercises/exo2/reader.go
+++ exercises/exo2/reader.go
@@ -10,6 +10,20 @@ type spaceEraser struct {
 	r io.Reader
 }
 
+func (s spaceEraser) Read(b []byte) (n int, err error) {
+	n, err = s.r.Read(b)
+	bModif := []byte{}
+	for _, c := range b {
+		if c != 32 {
+			bModif = append(bModif, c)
+		} else {
+			n--
+		}
+	}
+	copy(b, bModif)
+	return n, err
+}
+
 func main() {
 	s := strings.NewReader("H e l l o w o r l d!")
 	r := spaceEraser{s}
